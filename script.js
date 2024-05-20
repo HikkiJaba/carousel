20e2070a992f5ae346497126c2cb68b335912443
HikkiJaba <brait.2005@mail.ru>
2024-05-20 12:00:29 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const aside = document.querySelector('aside');
+
+    burgerMenu.addEventListener('click', function() {
+        this.classList.toggle('open');
+        aside.classList.toggle('open-aside');
+        document.querySelector('.options-panel').classList.remove('show');
+    });
+});
+
+
+
+function toggleOptionsPanel(panelId) {
+    var panel = document.getElementById('options-' + panelId);
+    var isOpen = panel.classList.contains('show');
+
+    var allPanels = document.querySelectorAll('.options-panel');
+    allPanels.forEach(function(panel) {
+        panel.classList.remove('show');
+    });
+
+    if (isOpen) {
+        panel.classList.remove('show');
+    } else {
+        panel.classList.add('show');
+    }
+}
+
+
+
+const iconBtns = document.querySelectorAll('.icon-btn');
+
+
+iconBtns.forEach(btn => {
+    btn.addEventListener('click', function() {
+        iconBtns.forEach(b => b.classList.remove('active-icon-btn'));
+        this.classList.add('active-icon-btn');
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const aside = document.querySelector('aside');
+    const iconButtons = document.querySelectorAll('.icon-btn');
+
+    burgerMenu.addEventListener('click', function() {
+        this.classList.toggle('open');
+        aside.classList.toggle('open');
+        document.querySelector('.options-panel').classList.remove('show');
+    });
+
+    iconButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            iconButtons.forEach(btn => {
+                btn.classList.remove('active');
+            });
+            if (this.classList.contains('active')) {
+                this.classList.remove('active');
+                document.querySelector('.options-panel').classList.remove('show');
+            } else {
+                this.classList.add('active');
+                document.querySelector('.options-panel').classList.add('show');
+            }
+        });
+    });
+});
