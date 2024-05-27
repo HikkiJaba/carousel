8fbef0f3b735be1ac8e7cb690b9916e31416a57d
HikkiJaba <brait.2005@mail.ru>
2024-05-27 22:36:06 +0700

    add form + 3d

--- client/script.js
+++ client/script.js
@@ -1,3 +1,224 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 150;
+
+var renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth * 0.8, window.innerHeight * 0.8);
+renderer.setClearColor(0x000000, 0);
+document.getElementById("main-container").appendChild(renderer.domElement);
+
+var light = new THREE.DirectionalLight(0xffffff);
+light.position.set(1, 1, 1).normalize();
+scene.add(light);
+
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
+scene.add(ambientLight);
+
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.enableZoom = false;
+controls.update();
+
+var rotationGroup = new THREE.Group();
+scene.add(rotationGroup); 
+
+function rotateModelAroundCenter(model) {
+    var boundingBox = new THREE.Box3().setFromObject(model);
+    var center = new THREE.Vector3();
+    boundingBox.getCenter(center);
+    model.position.copy(center.negate()); 
+}
+
+var currentModel;
+
+function loadModel(path, scaleFactor) {
+    if (currentModel) {
+        scene.remove(currentModel);
+    }
+    var loader = new THREE.OBJLoader();
+    loader.load(
+        path,
+        function (obj) {
+            var geometry = obj.children[0].geometry;
+
+            var box = new THREE.Box3().setFromObject(obj);
+            var size = new THREE.Vector3();
+            box.getSize(size);
+
+            var scale = Math.min(20.0 * 10 / size.x, 20.0 * 10 / size.y, 20.0 * 10 / size.z);
+
+            obj.position.y = -100;
+
+            obj.scale.set(scale, scale, scale);
+
+            scene.add(obj);
+            currentModel = obj;
+        },
+        function (xhr) {},
+        function (error) {
+            console.error("Произошла ошибка при загрузке модели", error);
+        }
+    );
+}
+
+var ledCheckbox = document.getElementById('ledCheckbox'); 
+var fancyEffectCheckbox = document.getElementById('fancyEffectCheckbox');
+var kineticCheckbox = document.getElementById('kineticCheckbox');
+
+ledCheckbox.addEventListener('change', function () {
+    updateModelBasedOnCheckboxes();
+});
+
+fancyEffectCheckbox.addEventListener('change', function () {
+    updateModelBasedOnCheckboxes();
+});
+
+kineticCheckbox.addEventListener('change', function () {
+    updateModelBasedOnCheckboxes();
+});
+
+
+
+
+function loadAdditionalModel(path, scaleFactor) {
+    console.log("Loading additional model from path:", path);
+
+    var loader = new THREE.OBJLoader();
+    loader.load(
+        path,
+        function(obj) {
+            console.log("Model loaded successfully");
+            var geometry = obj.children[0].geometry;
+
+            var box = new THREE.Box3().setFromObject(obj);
+            var size = new THREE.Vector3();
+            box.getSize(size);
+
+            var scale = Math.min(10.0 * 10 / size.x, 10.0 * 10 / size.y, 10.0 * 10 / size.z);
+
+            obj.position.y = -100;
+
+            obj.scale.set(scale, scale, scale);
+
+            obj.userData.isAdditionalModel = true;
+            obj.name = 'additionalModel';
+
+            scene.add(obj);
+        },
+        function(xhr) {
+        },
+        function(error) {
+        }
+    );
+}
+
+
+
+
+function updateModelBasedOnCheckboxes() {
+    var additionalModel = scene.getObjectByName('additionalModel');
+    var fancyEffectCheckbox = document.getElementById('fancyEffectCheckbox');
+    var ledCheckbox = document.getElementById('ledCheckbox');
+    var kineticCheckbox = document.getElementById('kineticCheckbox');
+    while (rotationGroup.children.length > 0) {
+        var child = rotationGroup.children[0];
+        rotationGroup.remove(child);
+        scene.remove(child);
+    }
+    
+    console.log("Checking checkboxes:", fancyEffectCheckbox.checked, ledCheckbox.checked, kineticCheckbox.checked);
+
+        if (fancyEffectCheckbox.checked && ledCheckbox.checked) {
+            if (additionalModel) {
+                rotationGroup.remove(additionalModel);
+                scene.remove(additionalModel);
+            }
+            loadAdditionalModel('./obj/lightkar.obj', 0.5);
+            if (kineticCheckbox.checked) {
+                anime(additionalModel);
+            }
+        } else if (fancyEffectCheckbox.checked) {
+            if (additionalModel) {
+                rotationGroup.remove(additionalModel);
+                scene.remove(additionalModel);
+            }
+            loadAdditionalModel('./obj/kar.obj', 0.5);
+        } else  {
+            if (additionalModel) {
+                rotationGroup.remove(additionalModel);
+                scene.remove(additionalModel);
+            }
+        }
+}
+
+function anime() {
+    var kineticCheckbox = document.getElementById('kineticCheckbox');
+    var additionalModel = scene.getObjectByName('additionalModel');
+    if (kineticCheckbox.checked && additionalModel) {
+        additionalModel.rotation.y -= 0.01;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+loadModel("./obj/telokar.obj", 0.8);
+
+function observePanel(panelId) {
+    var panel = document.getElementById(panelId);
+    var observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            if (mutation.attributeName === "class") {
+                var isVisible = panel.classList.contains('show');
+                if (isVisible) {
+                    loadModel('./obj/flex1.obj', 1);
+                }
+            }
+        });
+    });
+
+    observer.observe(panel, { attributes: true });
+}
+
+observePanel("options-size");
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+    anime();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var fancyEffectCheckbox = document.getElementById('fancyEffectCheckbox');
+    var ledCheckbox = document.getElementById('ledCheckbox');
+
+    fancyEffectCheckbox.addEventListener('change', updateModelBasedOnCheckboxes);
+    ledCheckbox.addEventListener('change', updateModelBasedOnCheckboxes);
+
+    ledCheckbox.addEventListener('change', function () {
+        updateModelBasedOnCheckboxes();
+    });
+
+    fancyEffectCheckbox.addEventListener('change', function () {
+        if (this.checked) {
+            loadAdditionalModel('./obj/kar.obj', 0.5); 
+        } else {
+            var additionalModel = scene.getObjectByName('additionalModel');
+            if (additionalModel) {
+                scene.remove(additionalModel);
+            }
+        }
+    });
+});
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const burgerMenu = document.querySelector('.burger-menu');
     const aside = document.querySelector('aside');
@@ -29,7 +250,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
 function toggleOptionsPanel(panelId) {
     var panel = document.getElementById('options-' + panelId);
     var isOpen = panel.classList.contains('show');
@@ -46,9 +266,7 @@ function toggleOptionsPanel(panelId) {
     }
 }
 
-
 const iconBtns = document.querySelectorAll('.icon-btn');
-
 
 iconBtns.forEach(btn => {
     btn.addEventListener('click', function() {
@@ -90,5 +308,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 setTimeout(() => {
     document.querySelector('#main-container>canvas').style.cursor = 'grab'
-}, 100)
+}, 100);
 
+animate();
